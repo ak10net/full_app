74c13d8d5cfb5c9c4b9f689db8da83f11fb5406e
ak10net <ankit.dubey@entercoms.com>
2021-02-20 11:04:03 +0530

    add template body and tokenizer-workingapp

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,6 +17,44 @@ db = SQLAlchemy(app)
 
 from models import Result
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	errors = []
+	results = {}
+	if request.method == 'POST':
+		try:
+			url = request.form['url']
+			r = requests.get(url)
+		except:
+			errors.append('Unable to get URL')
+		if r:
+			raw = BeautifulSoup(r.text, 'html.parser').get_text()
+			nltk.data.path.append('./nltk_data')
+			tokens = nltk.word_tokenize(raw)
+			text = nltk.Text(tokens)
+			nonPunct = re.compile('.[A_Za-z].*')
+			raw_words = [w for w in text if nonPunct.match(w)]
+			raw_word_count = Counter(raw_words)
+			no_stop_words = [w for w in raw_words if w.lower() not in stops]
+			no_stop_words_count = Counter(no_stop_words)
+			# save the results
+			results = sorted(
+				no_stop_words_count.items(),
+				key=operator.itemgetter(1),
+				reverse=True
+			)
+			try:
+				result = Result(
+					url=url,
+					result_all=raw_word_count,
+					result_no_stop_words=no_stop_words_count
+				)
+				db.session.add(result)
+				db.session.commit()
+			except:
+				errors.append('Unable to add item to database')
+	return render_template('index.html', errors=errors, results=results)
+'''
 @app.route('/')
 def hello():
 	return 'Hello World!'
@@ -20,6 +65,7 @@ def hello_name(name):
 	return 'Hello {}!'.format(name)
 
 #print(os.environ['APP_SETTINGS'])
+'''
 
 if __name__ == '__main__':
 	app.run()
